d6d3ff33148af01a33bee0d15a8ddc463b14ef05
Abdullah Al Mamun <abdullah914115@gmail.com>
2019-11-19 22:47:33 +0600

    FitNess
    Full responsive v-1.0.2

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(".navbar_burgur").click(function () {
     $("nav.nav").toggle();
 });
 $(window).resize(function(){
-    if(window.innerWidth >=920){
+    if(window.innerWidth >=1082){
         $("nav.nav").css("display","flex");
     }else{
         $("nav.nav").css("display", "none");
